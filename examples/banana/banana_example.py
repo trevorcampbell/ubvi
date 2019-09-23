8995411562752e96ec88cccb081223d37eeb0a4f
xinglong-li <xinglong.li@stat.ubc.ca>
2019-09-22 17:12:59 -0700

    adjust parameters according to ubvi and bbvi

--- examples/banana/banana_example.py
+++ examples/banana/banana_example.py
@@ -3,6 +3,7 @@ import pickle as pk
 import os
 
 from distributions import Gaussian
+from optimizations import Adam
 from ubvi import UBVI
 from bbvi import BBVI
 
@@ -35,16 +36,17 @@ if not os.path.exists('results/'):
   os.mkdir('results')
 
 gauss = Gaussian(d, diag)
+adam = Adam(adam_learning_rate, adam_num_iters, print_every)
 
 for i in range(N_runs):
   print('RUN ' + str(i+1)+'/'+str(N_runs))
-  ubvi = UBVI(logf, N, gauss, n_samples, n_logfg_samples, n_init, adam_learning_rate, adam_num_iters, print_every)
+  ubvi = UBVI(logf, N, gauss, adam, n_samples, n_init, n_logfg_samples)
   banana_ubvi = ubvi.build()
 
-  bbvi = BBVI(banana, N, gauss, n_samples, n_init, lmb_good, adam_learning_rate, adam_num_iters, print_every)
+  bbvi = BBVI(banana, N, gauss, adam, n_samples, n_init, lmb_good)
   banana_bbvi = bbvi.build()
 
-  bbvi2 = BBVI(banana, N, gauss, n_samples, n_init, lmb_bad, adam_learning_rate, adam_num_iters, print_every)
+  bbvi2 = BBVI(banana, N, gauss, adam, n_samples, n_init, lmb_bad)
   banana_bbvi2 = bbvi2.build()
 
   if os.path.exists('results/banana.pk'):
