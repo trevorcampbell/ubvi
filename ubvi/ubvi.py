7041d3669552f688e9710d0575a52ffda05c1bfc
xinglong-li <xinglong.li@stat.ubc.ca>
2019-09-23 22:52:58 -0700

    remove initialize function; fix a bug in sample_g

--- ubvi/ubvi.py
+++ ubvi/ubvi.py
@@ -10,7 +10,7 @@ class UBVI(BoostingVI):
         super().__init__(logf, N, distribution, optimization)
         self.n_samples = n_samples
         self.n_init = n_init
-        self.init_inflation = 1
+        self.init_inflation = 100
         self.n_logfg_samples = n_logfg_samples
         self.Z = np.zeros((self.N, self.N))
         self._logfg = -np.inf*np.ones(self.N)
@@ -21,6 +21,7 @@ class UBVI(BoostingVI):
         self.Z[i, :i+1] = Znew
         self.Z[:i+1, i] = Znew
         self._logfg[i] = self._logfg_est(i)
+        print('Updating weights...')
         if i == 0:
             return 1
         else:
@@ -31,12 +32,16 @@ class UBVI(BoostingVI):
             return np.maximum(0., np.dot(Linv.T, lbd/np.sqrt(((lbd**2).sum()))))
         
     def _current_distance(self, i):
+        print('New weights: ' + str(self.g_w[:i+1]))
         #compute current log<f, g> estimate
         self._logfgsum = logsumexp(np.hstack((-np.inf, self._logfg[:i+1] + np.log(np.maximum(self.g_w[:i+1], 1e-64)))))
         hellsq = self._hellsq_est(i)
         print('New Hellinger-Squared estimate: ' + str(hellsq))
     
     def _objective(self, x, Params, W):
+        '''
+        Returns the negative of the objective function and should be minimized.
+        '''
         lgh = -np.inf if W.shape[0] == 0 else logsumexp(np.log(np.maximum(W, 1e-64)) + self.D.log_sqrt_pair_integral(x, Params))
         #get samples from h
         h_samples = self.D.sample(x, self.n_samples)
@@ -48,10 +53,11 @@ class UBVI(BoostingVI):
         log_denom = 0.5*np.log(1.-np.exp(2*lgh))
         if lf_num > lg_num:
             logobj = lf_num - log_denom + np.log(1.-np.exp(lg_num-lf_num))
-            return logobj
+            neglogobj = -logobj
+            return neglogobj
         else:
-            logobj = lg_num - log_denom + np.log(1.-np.exp(lf_num-lg_num))
-            return -logobj
+            lognegobj = lg_num - log_denom + np.log(1.-np.exp(lf_num-lg_num))
+            return lognegobj
             
     def _hellsq_est(self, i):
         samples = self._sample_g(i, self.n_samples)
@@ -85,33 +91,17 @@ class UBVI(BoostingVI):
         pair_samples = np.random.multinomial(n, g_ps).reshape((i+1, i+1))
         #symmetrize (will just use lower triangular below for efficiency)
         pair_samples = pair_samples + pair_samples.T
-        for k in range(i):
+        for k in range(i+1):
             pair_samples[k,k] /= 2
         #invert sigs
         #fill in samples
         cur_idx = 0
-        for j in range(i):
+        for j in range(i+1):
             for m in range(j+1):
                 n_samps = pair_samples[j,m]
                 g_samples[cur_idx:cur_idx+n_samps, :] = self.D.cross_sample(self.params[j], self.params[m], n_samps)
                 cur_idx += n_samps
         return g_samples
-    
-    def _initialize(self, obj, i):
-        print("Initializing ... ")
-        x0 = None
-        obj0 = np.inf
-        for n in range(self.n_init):
-            xtmp = self.D.params_init(self.params[:i, :], self.g_w[:i], self.init_inflation)
-            objtmp = obj(xtmp)
-            if objtmp < obj0:
-                x0 = xtmp
-                obj0 = objtmp
-                print('improved x0: ' + str(x0) + ' with obj0 = ' + str(obj0))
-        if x0 is None:
-            raise ValueError
-        else:
-            return x0
-        
-        
+       
+       
         
