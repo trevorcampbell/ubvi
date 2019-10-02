6c99d2ed7d8b70e1b1ac9a665f6bd2e9e9c9bfb3
xinglong-li <xinglong.li@stat.ubc.ca>
2019-10-02 11:29:18 -0700

    get rid of dependencies on indexing 'i'

--- ubvi/boostingvi.py
+++ ubvi/boostingvi.py
@@ -10,40 +10,40 @@ class BoostingVI(object):
         self.N = N
         self.D = distribution
         self.Opt = optimization
-        self.g_w = np.zeros(N)
+        self.g_w = np.empty((0,0))
         self.G_w = []
-        self.params = np.zeros((N, self.D.dim))
+        self.params = np.empty((0, self.D.dim))
         self.components = None
         self.cput = np.zeros(N) 
         
     def build(self):
         for i in range(self.N):
             t0 = time.process_time()
-            optimized_params = self._new_component(i)
-            self.params[i] = optimized_params
-            self.g_w[:i+1] = self._new_weights(i)
-            self.G_w.append(self.g_w[:i+1])
-            self._current_distance(i)
+            optimized_params = self._new_component()
+            self.params = np.vstack((self.params, optimized_params))
+            self.g_w = np.atleast_1d(self._weights_update())
+            self.G_w.append(self.g_w)
+            self._current_distance()
             self.cput[i] = time.process_time() - t0
         self.components = self.D.reparam(self.params)
         output = self.components
         output.update([('g_w', self.g_w), ('G_w', self.G_w), ('cput', self.cput)])
         return output
         
-    def _new_component(self, i):
-        obj = lambda x, itr: self._objective(x, self.params[:i], self.g_w[:i])
-        x0 = self._initialize(obj, i)
+    def _new_component(self):
+        obj = lambda x, itr: self._objective(x, itr)
+        x0 = self._initialize(obj)
         grd = grad(obj)
         opt_params = self.Opt.optimize(grd, x0, callback=lambda prms, itr, grd : self.D.print_perf(prms, itr, grd, self.Opt.print_every, obj))
         print("Comoponent optimization complete.")
         return opt_params
     
-    def _initialize(self, obj, i):
+    def _initialize(self, obj):
         print("Initializing ... ")
         x0 = None
         obj0 = np.inf
         for n in range(self.n_init):
-            xtmp = self.D.params_init(self.params[:i], self.g_w[:i], self.init_inflation)
+            xtmp = self.D.params_init(self.params, self.g_w, self.init_inflation)
             objtmp = obj(xtmp, -1)
             if objtmp < obj0:
                 x0 = xtmp
@@ -57,7 +57,7 @@ class BoostingVI(object):
     def _objective(self):
         raise NotImplementedError
         
-    def _new_weights(self, i):
+    def _new_weights(self):
         raise NotImplementedError
     
     def _current_distance(self):
