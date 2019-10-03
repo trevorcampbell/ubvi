b3ad358e9d35df17acff1f447c6db0ea6db6f68d
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-10-03 11:51:23 -0700

    fixing opt bugs

--- ubvi/optimization/adam.py
+++ ubvi/optimization/adam.py
@@ -1,19 +1,19 @@
 import autograd.numpy as np
 
-def adam(self, grd, x, learning_rate, num_iters, print_every, callback=None):
+def adam(grd, x, learning_rate, num_iters, callback=None):
     b1=0.9
     b2=0.999
     eps=10**-8
     m = np.zeros(len(x))
     v = np.zeros(len(x))
-    for i in range(self.num_iters):
+    for i in range(num_iters):
         g = grd(x, i)
         if callback: callback(x, i, g)
         m = (1 - b1) * g + b1 * m  # First  moment estimate.
         v = (1 - b2) * (g**2) + b2 * v  # Second moment estimate.
         mhat = m / (1 - b1**(i + 1))    # Bias correction.
         vhat = v / (1 - b2**(i + 1))
-        x = x - self.learning_rate(i)*mhat/(np.sqrt(vhat) + eps)
+        x = x - learning_rate(i)*mhat/(np.sqrt(vhat) + eps)
     return x
     
     
