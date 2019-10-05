b5042cdfe206237bbfa7820807ef39e2dad4c2d5
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-10-04 22:23:16 -0700

    fixing cauchy/banana examples for new code

--- examples/banana/main.py
+++ examples/banana/main.py
@@ -2,64 +2,64 @@ import autograd.numpy as np
 import pickle as pk
 import os
 
-from distributions import Gaussian
-from optimizations import Adam
-from ubvi import UBVI
-from bbvi import BBVI
+from ubvi.components import Gaussian
+from ubvi.optimization import adam as ubvi_adam
+from ubvi.inference import UBVI, BBVI
+from ubvi.autograd import logsumexp
 
-
-def banana(X):
+def logp(X):
     b = 0.1
     x = X[:,0]
     y = X[:,1]
     return -x**2/200 - (y+b*x**2-100*b)**2/2 - np.log(2*np.pi*10)
 
-def logf(x):
-   return 0.5*banana(x)
-
-
-
 N_runs = 1
 N = 3
-d = 2
+d = 1
 diag = False
-n_samples = 100
+n_samples = 500
 n_logfg_samples = 10000
-adam_num_iters = 3000
-adam_learning_rate = lambda itr : 0.1/np.sqrt(1.+itr)
-n_init=1000
-lmb_good = lambda itr : 70./(itr+1)
-lmb_bad = lambda itr : 1./(itr+1)
-print_every=100
+adam_learning_rate= lambda itr : 1./(itr+1)
+adam_num_iters = 5000
+n_init = 10000
+init_inflation = 100
+lmb_good = lambda itr : 1./(1+itr)
+lmb_bad = lambda itr : 70./(1+itr)
+
+
+gauss = Gaussian(d, diag)
+adam = lambda x0, obj, grd : ubvi_adam(x0, obj, grd, adam_learning_rate, adam_num_iters, callback = gauss.print_perf)
+
 
 if not os.path.exists('results/'):
   os.mkdir('results')
 
-gauss = Gaussian(d, diag)
-adam = Adam(adam_learning_rate, adam_num_iters, print_every)
-
 for i in range(N_runs):
-  print('RUN ' + str(i+1)+'/'+str(N_runs))
-  ubvi = UBVI(logf, N, gauss, adam, n_samples, n_init, n_logfg_samples)
-  banana_ubvi = ubvi.build()
 
-  bbvi = BBVI(banana, N, gauss, adam, n_samples, n_init, lmb_good)
-  banana_bbvi = bbvi.build()
+    #UBVI
+    ubvi = UBVI(logp, gauss, adam, n_init = n_init, n_samples = n_samples, n_logfg_samples = n_logfg_samples, init_inflation = init_inflation)
+    banana_ubvi = ubvi.build(N)
 
-  bbvi2 = BBVI(banana, N, gauss, adam, n_samples, n_init, lmb_bad)
-  banana_bbvi2 = bbvi2.build()
+    #BBVI Good
+    bbvi_good = BBVI(logp, gauss, adam, lmb = lmb_good, n_init = n_init, n_samples = n_samples, init_inflation = init_inflation)
+    banana_bbvi_good = bbvi_good.build(N)
 
-  if os.path.exists('results/banana.pk'):
-      f = open('results/banana.pk', 'rb')
-      res = pk.load(f)
-      f.close()
-      res[0].append(banana_ubvi)
-      res[1].append(banana_bbvi)
-      res[2].append(banana_bbvi2)
-      f = open('results/banana.pk', 'wb')
-      pk.dump(res, f)
-      f.close()
-  else:
-      f = open('results/banana.pk', 'wb')
-      pk.dump((banana_ubvi, banana_bbvi, banana_bbvi2), f)
-      f.close()
+    #BBVI Bad
+    bbvi_bad = BBVI(logp, gauss, adam, lmb = lmb_bad, n_init = n_init, n_samples = n_samples, init_inflation = init_inflation)
+    banana_bbvi_bad = bbvi_bad.build(N)
+
+    if os.path.exists('results/banana.pk'):
+        f = open('results/banana.pk', 'rb')
+        res = pk.load(f)
+        f.close()
+        res[0].append(banana_ubvi)
+        res[1].append(banana_bbvi_good)
+        res[2].append(banana_bbvi_bad)
+        f = open('results/banana.pk', 'wb')
+        pk.dump(res, f)
+        f.close()
+    else:
+        f = open('results/banana.pk', 'wb')
+        pk.dump(([banana_ubvi], [banana_bbvi_good], [banana_bbvi_bad]), f)
+        f.close()
+
--- examples/cauchy/main.py
+++ examples/cauchy/main.py
@@ -2,63 +2,61 @@ import autograd.numpy as np
 import pickle as pk
 import os
 
-from distributions import Gaussian
-from optimizations import Adam
-from ubvi import UBVI
-from bbvi import BBVI
+from ubvi.components import Gaussian
+from ubvi.optimization import adam as ubvi_adam
+from ubvi.inference import UBVI, BBVI
+from ubvi.autograd import logsumexp
 
-
-def cauchy(x):
+def logp(x):
     return (- np.log(1 + x**2) - np.log(np.pi)).flatten()
-
-def logf(x):
-   return 0.5*cauchy(x)
-
 
 N_runs = 1
 N = 3
 d = 1
-diag=False
-n_samples = 100
+diag = False
+n_samples = 500
 n_logfg_samples = 10000
+adam_learning_rate= lambda itr : 1./(itr+1)
 adam_num_iters = 5000
-adam_learning_rate = lambda itr : 0.1/np.sqrt(1.+itr)
-n_init=1000
+n_init = 10000
+init_inflation = 100
 lmb_good = lambda itr : 1./(1+itr)
 lmb_bad = lambda itr : 70./(1+itr)
-print_every = 100
 
+
+gauss = Gaussian(d, diag)
+adam = lambda x0, obj, grd : ubvi_adam(x0, obj, grd, adam_learning_rate, adam_num_iters, callback = gauss.print_perf)
 
 
 if not os.path.exists('results/'):
     os.mkdir('results')
 
-gauss = Gaussian(d, diag)
-adam = Adam(adam_learning_rate, adam_num_iters, print_every)
-
 for i in range(N_runs):
-    print('RUN ' + str(i+1)+'/'+str(N_runs))
-    
-    ubvi = UBVI(logf, N, gauss, adam, n_samples, n_init, n_logfg_samples)
-    cauchy_ubvi = ubvi.build()
-    
-    bbvi = BBVI(cauchy, N, gauss, adam, n_samples, n_init, lmb_good)
-    cauchy_bbvi = bbvi.build()
-    
-    bbvi2 = BBVI(cauchy, N, gauss, adam, n_samples, n_init, lmb_bad)
-    cauchy_bbvi2 = bbvi2.build()
-    
+    print('Run ' + str(i+1)+'/'+str(N_runs))
+
+    #UBVI
+    ubvi = UBVI(logp, gauss, adam, n_init = n_init, n_samples = n_samples, n_logfg_samples = n_logfg_samples, init_inflation = init_inflation)
+    cauchy_ubvi = ubvi.build(N)
+
+    #BBVI Good
+    bbvi_good = BBVI(logp, gauss, adam, lmb = lmb_good, n_init = n_init, n_samples = n_samples, init_inflation = init_inflation)
+    cauchy_bbvi_good = bbvi_good.build(N)
+
+    #BBVI Bad
+    bbvi_bad = BBVI(logp, gauss, adam, lmb = lmb_bad, n_init = n_init, n_samples = n_samples, init_inflation = init_inflation)
+    cauchy_bbvi_bad = bbvi_bad.build(N)
+
     if os.path.exists('results/cauchy.pk'):
         f = open('results/cauchy.pk', 'rb')
         res = pk.load(f)
         f.close()
         res[0].append(cauchy_ubvi)
-        res[1].append(cauchy_bbvi)
-        res[2].append(cauchy_bbvi2)
+        res[1].append(cauchy_bbvi_good)
+        res[2].append(cauchy_bbvi_bad)
         f = open('results/cauchy.pk', 'wb')
         pk.dump(res, f)
         f.close()
     else:
         f = open('results/cauchy.pk', 'wb')
-        pk.dump((cauchy_ubvi, cauchy_bbvi, cauchy_bbvi2), f)
+        pk.dump(([cauchy_ubvi], [cauchy_bbvi_good], [cauchy_bbvi_bad]), f)
         f.close()
